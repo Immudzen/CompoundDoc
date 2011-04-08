d5f87bffbf01346541d3181f7697081252616433
William Heymann <kosh@aesaeion.com>
2011-04-07 18:06:28 -0600

    makeThumbnail no longer takes a filename, it creates a temp file when it needs to added more extension types to the lookup added bobo_traverse to deal with unique image urls added unique url generation to absolute_url_path_extension created absolute_url_path_extension_thumbnail

--- basepicture.py
+++ basepicture.py
@@ -47,7 +47,7 @@ class BasePicture(File):
     configurable = ('format', 'color',)
 
     security.declarePrivate('makeThumbnail')
-    def makeThumbnail(self, filename):
+    def makeThumbnail(self):
         """
         Makes a thumbnail image given an image Id when called on a Zope
         folder.
@@ -58,29 +58,41 @@ class BasePicture(File):
         object.
         """
         size=30
-        try:
-            from PIL import Image
-                        
-            image=Image.open(filename)
-            image=image.convert('RGB')
-            (x,y) = image.size
-            if x > size: x = size
-            if y > size: y = size
-            image = image.resize((x,y))
-            thumbnail_file = utility.saveImage(image, 'JPEG')
-            thumbnail_id = "thumbnail"
-            self.setObject(thumbnail_id, OFS.Image.Image(thumbnail_id, thumbnail_id, thumbnail_file))
-        except (IOError, ImportError):
-            pass
+        data = self.data or self.image
+        filename, remove_after = utility.createTempFile(data.data)
+        content_type = magicfile.magic(filename)
+        if content_type.startswith('image'):
+            try:
+                from PIL import Image
+                            
+                image=Image.open(filename)
+                image=image.convert('RGB')
+                (x,y) = image.size
+                if x > size: x = size
+                if y > size: y = size
+                image = image.resize((x,y))
+                thumbnail_file = utility.saveImage(image, 'JPEG')
+                thumbnail_id = "thumbnail"
+                self.setObject(thumbnail_id, OFS.Image.Image(thumbnail_id, thumbnail_id, thumbnail_file))
+            except (IOError, ImportError):
+                pass
+        utility.removeTempFile(filename, remove_after) 
 
     extensionLookup = {}
     extensionLookup['image/jpeg'] = 'jpg'
+    extensionLookup['image/jpeg; charset=binary'] = 'jpg'
     extensionLookup['image/png'] = 'png'
+    extensionLookup['image/png; charset=binary'] = 'png'
     extensionLookup['image/gif'] = 'gif'
+    extensionLookup['image/gif; charset=binary'] = 'gif'
     
     security.declarePublic("__bobo_traverse__")
     def __bobo_traverse__(self, REQUEST, name):
         "__bobo_traverse__"
+        if name.startswith('ver_'):
+            name = self.REQUEST.TraversalRequestNameStack.pop()
+            self.REQUEST.RESPONSE.setHeader('Cache-Control', 'max-age=315360000')
+            self.REQUEST.RESPONSE.setHeader('Expires', 'Thu, 01 Dec 2030 12:00:0')
         obj = getattr(self, name, None)
         if obj is not None:
             return obj
@@ -109,25 +121,29 @@ class BasePicture(File):
     security.declareProtected('View', 'absolute_url_path_extension')
     def absolute_url_path_extension(self):
         "return the url to this image plus the filename and extension"
-        return self.absolute_url_path() + '/' + self.getImageNameExtension()
+        data = self.data or self.image
+        return self.absolute_url_path() + '/ver_%s/' % int(data.bobobase_modification_time().timeTime()) + self.getImageNameExtension()
+
+    security.declareProtected('View', 'absolute_url_path_extension_thumbnail')
+    def absolute_url_path_extension_thumbnail(self):
+        "return the url to this image plus the filename and extension"
+        data = self.thumbnail
+        return self.absolute_url_path() + '/ver_%s/thumbnail' % int(data.bobobase_modification_time().timeTime())
 
     security.declareProtected('View', 'absolute_url_extension')
     def absolute_url_extension(self):
         "return the url to this image plus the filename and extension"
-        return self.absolute_url() + '/' + self.getImageNameExtension()
+        data = self.data or self.image
+        return self.absolute_url() + '/ver_%s/' % int(data.bobobase_modification_time().timeTime()) + self.getImageNameExtension()
 
     security.declarePrivate('small')
     def small(self):
         "Draw a small version of the object"
         if self.exists() and self.thumbnail is None:            
-            filename, remove_after = utility.createTempFile(self.data.data)
-            content_type = magicfile.magic(filename)
-            if content_type.startswith('image'):
-                self.makeThumbnail(filename)
-            utility.removeTempFile(filename, remove_after)
+            self.makeThumbnail()
 
         if self.exists() and self.hasObject('thumbnail'):
-            decode = {'url': self.thumbnail.absolute_url_path(),
+            decode = {'url': self.absolute_url_path_extension_thumbnail(),
               'fullUrl':self.absolute_url_path_extension(),
               'height':self.thumbnail.height ,
               'width':self.thumbnail.width ,
