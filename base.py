ba118aecd9ee1e12e921f425f512c5c2c051a30b
William Heymann <kosh@aesaeion.com>
2011-04-20 03:41:48 -0600

    new upgrader version

--- base.py
+++ base.py
@@ -41,7 +41,7 @@ class Base(baseobject.BaseObject,
   SimpleItem):
     "Abstract Base"
 
-    classVersion = 170  #run upgrader scripts 160
+    classVersion = 173  #run upgrader scripts 160
     classUpdateVersion = 122  #add attributes
     overwrite = 0
     updateAlways = 0
