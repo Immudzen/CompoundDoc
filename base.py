0650600282505b07b73e3a95b6838eb0fbc47354
William Heymann <kosh@aesaeion.com>
2011-05-08 23:13:43 -0600

    new classVersion and also got rid of _getOb since it is not used

--- base.py
+++ base.py
@@ -41,7 +41,7 @@ class Base(baseobject.BaseObject,
   SimpleItem):
     "Abstract Base"
 
-    classVersion = 174  #run upgrader scripts 160
+    classVersion = 176  #run upgrader scripts 160
     classUpdateVersion = 122  #add attributes
     overwrite = 0
     updateAlways = 0
@@ -67,7 +67,6 @@ class Base(baseobject.BaseObject,
 
     security.declareProtected("Access contents information", 'objectIds')
     objectIds = ObjectManager.objectIds
-    _getOb = ObjectManager._getOb
 
     security.declareProtected("Access contents information", 'objectIdsWithMetaType')
     def objectIdsWithMetaType(self, spec=None):
